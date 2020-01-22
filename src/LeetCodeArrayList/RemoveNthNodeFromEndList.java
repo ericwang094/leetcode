fcb0e62460375a2f3800c7af87a0df4f7e896ede
Eric <eric.wang094@gmail.com>
2020-01-22 12:21:08 +1100

    Merge branch 'master' of https://github.com/ericwang094/leetcode
    # Conflicts:
    #	src/DivideConquer/Test.java

--- src/LeetCodeArrayList/RemoveNthNodeFromEndList.java
+++ src/LeetCodeArrayList/RemoveNthNodeFromEndList.java
@@ -26,6 +26,27 @@ public class RemoveNthNodeFromEndList {
 		return dummy.next;
 	}
 
+	public ListNode removeNthFromEnd2(ListNode head, int n) {
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+
+		ListNode fast = dummy;
+		ListNode slow = dummy;
+
+		for (int i = 0; i <= n; i++) {
+			fast = fast.next;
+		}
+
+		while (fast != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+
+		slow.next = slow.next.next;
+		return dummy.next;
+
+	}
+
 	public static void main(String[] args) {
 		RemoveNthNodeFromEndList test = new RemoveNthNodeFromEndList();
 //		ListNode input = new ListNode(1);
