93fa78b3c8a51220d79bd941a161a10a5877810d
ericwang094 <eric.wang094@gmail.com>
2020-02-03 09:30:09 +1100

    Merge branch 'master' of https://github.com/ericwang094/leetcode
    # Conflicts:
    #	src/DivideConquer/Test.java

---
+++ src/LeetCodeArrayList/InsertionSortList.java
@@ -0,0 +1,41 @@
+package LeetCodeArrayList;
+
+import TwoPointers.ListNode;
+
+public class InsertionSortList {
+	public ListNode insertionSortList(ListNode head) {
+		if( head == null ){
+			return head;
+		}
+
+		ListNode helper = new ListNode(0); //new starter of the sorted list
+		ListNode cur = head; //the node will be inserted
+		ListNode pre = helper; //insert node between pre and pre.next
+		ListNode next = null; //the next node will be inserted
+		//not the end of input list
+		while( cur != null ){
+			next = cur.next;
+			//find the right place to insert
+			while( pre.next != null && pre.next.val < cur.val ){
+				pre = pre.next;
+			}
+			//insert between pre and pre.next
+			cur.next = pre.next;
+			pre.next = cur;
+			pre = helper;
+			cur = next;
+		}
+
+		return helper.next;
+	}
+
+	public static void main(String[] args) {
+		ListNode node = new ListNode(4);
+		node.next = new ListNode(2);
+		node.next.next = new ListNode(1);
+		node.next.next.next = new ListNode(3);
+
+		InsertionSortList test = new InsertionSortList();
+		test.insertionSortList(node);
+	}
+}
