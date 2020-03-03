686b54f3de7630c0c81741d21b266fa49900d64a
ericwang094 <eric.wang094@gmail.com>
2020-03-03 23:23:52 +1100

    connection graph

---
+++ src/UnionFind/ConnectingGraph.java
@@ -0,0 +1,52 @@
+package UnionFind;
+
+public class ConnectingGraph {
+	int[] graph;
+	/*
+	 * @param n: An integer
+	 */
+	public ConnectingGraph(int n) {
+		// do intialization if necessary
+		this.graph = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			graph[i] = i;
+		}
+	}
+
+	private int find(int node) {
+	 	int father = this.graph[node];
+	 	while (father != this.graph[father]) {
+	 		father = graph[father];
+	    }
+
+	    int temp = 0;
+		while (node != father) {
+	 		temp = graph[node];
+	 		graph[temp] = father;
+	 		node = temp;
+		}
+
+		return father;
+	}
+	/*
+	 * @param a: An integer
+	 * @param b: An integer
+	 * @return: nothing
+	 */
+	public void connect(int a, int b) {
+		// write your code here
+		int findA = find(a);
+		int findB = find(b);
+		this.graph[findA] = findB;
+	}
+
+	/*
+	 * @param a: An integer
+	 * @param b: An integer
+	 * @return: A boolean
+	 */
+	public boolean query(int a, int b) {
+		// write your code here
+		return find(a) == find(b);
+	}
+}
